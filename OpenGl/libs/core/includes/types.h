d1e0efdd34aaecd0162c80a286343d0cfd883715
in4a21 <in4a21@ILI11>
2021-10-23 10:15:37 +0200

    Aggiungere i file di progetto.

---
+++ OpenGl/libs/core/includes/types.h
@@ -0,0 +1,15 @@
+#pragma once
+
+using i8  = __int8;
+using i16 = __int16;
+using i32 = __int32;
+using i64 = __int16;
+using u8  = unsigned __int8;
+using u16 = unsigned __int16;
+using u32 = unsigned __int32;
+using u64 = unsigned __int64;
+using f32 = float;
+using f64 = double;
+using byte = u8;
+using usize = size_t;
+using isize = signed long long;
---
+++ OpenGl/src/os/window.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <GLFW/glfw3.h>
+
+class Window {
+private:
+	GLFWwindow* glfw_window;
+
+public:
+	Window();
+	~Window();
+
+	
+};
+
